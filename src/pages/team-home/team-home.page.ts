835885e2ac4cc575de89e0ca14cf9d433c676347
Karim Masani <karimmasani@karims-mbp.leoguest.fg.rbc.com>
2016-12-02 17:24:31 -0500

    commit

--- src/pages/team-home/team-home.page.ts
+++ src/pages/team-home/team-home.page.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
-import { StandingsPage, TeamDetailPage } from '../pages';
+import { MyTeamsPage, StandingsPage, TeamDetailPage } from '../pages';
 
 @Component({
   selector: 'page-team-home',
   templateUrl: 'team-home.page.html'
 })
 export class TeamHomePage {
-
+  team: any;
   teamDetailTab = TeamDetailPage;
   standingsTab = StandingsPage;
 
-  constructor(private navCtrl: NavController) {}
+  constructor(private navCtrl: NavController, private navParams: NavParams) {
+    this.team = this.navParams.data;
+  }
+
+  goHome() {
+    this.navCtrl.push(MyTeamsPage);
+  }
 }
