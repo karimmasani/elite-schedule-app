8d00587075ef65675a4b4679c90ee48a010e745e
karim.masani <karim.masani@outlook.com>
2016-12-06 14:16:36 -0500

    commit

--- src/pages/tournaments/tournaments.page.ts
+++ src/pages/tournaments/tournaments.page.ts
@@ -2,17 +2,23 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { TeamsPage } from '../pages';
+import { EliteApi } from '../../shared/shared';
 
 @Component({
   selector: 'page-tournaments',
   templateUrl: 'tournaments.page.html'
 })
 export class TournamentsPage {
+  tournaments: any;
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController, private eliteApi: EliteApi) {}
 
-  itemTapped() {
-    this.navCtrl.push(TeamsPage);
+  itemTapped($event, tourney) {
+    this.navCtrl.push(TeamsPage, tourney);
+  }
+
+  ionViewLoaded() {
+    this.eliteApi.getTournaments().then(data => this.tournaments = data);
   }
 
 }
